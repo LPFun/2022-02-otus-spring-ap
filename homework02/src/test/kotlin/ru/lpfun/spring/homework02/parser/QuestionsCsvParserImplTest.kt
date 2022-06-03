5bd0adccb7a10a79061f529b5ab29044670be6bf
Alexandr Popovich <a.popovich@alfaleads.net>
2022-06-03 22:09:14 +0300

    add parser test

---
+++ homework02/src/test/kotlin/ru/lpfun/spring/homework02/parser/QuestionsCsvParserImplTest.kt
@@ -0,0 +1,25 @@
+package ru.lpfun.spring.homework02.parser
+
+import org.junit.jupiter.api.Assertions.assertNotNull
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.test.context.junit.jupiter.SpringExtension
+
+@ExtendWith(SpringExtension::class)
+internal class QuestionsCsvParserImplTest {
+    @Test
+    fun `Question csv parser`() {
+        val parser = QuestionsCsvParserImpl()
+        val questions = parser.parse("data/questions.csv")
+
+        assertTrue(questions.size == 5)
+        val firstQuestion = questions.first()
+        firstQuestion.run {
+            assertNotNull(number)
+            assertNotNull(question)
+            assertNotNull(answerVariants)
+            assertNotNull(correctAnswers)
+        }
+    }
+}
