286db45e67da238f7f8f9f243ad14f4a49cfdd0e
Alexandr Popovich <a.popovich@alfaleads.net>
2022-06-08 21:29:19 +0300

    feature/homework03 init project

---
+++ homework03/src/main/kotlin/ru/lpfun/spring/homework03/Application.kt
@@ -0,0 +1,11 @@
+package ru.lpfun.spring.homework03
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Application
+
+fun main(args: Array<String>) {
+	runApplication<Application>(*args)
+}
