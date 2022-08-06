4cd697be3fb81bcdf1264609da5acd80fd665a37
Alexandr Popovich <a.popovich@alfaleads.net>
2022-08-06 20:32:44 +0300

    feature/homework05-spring-jdbc init project

---
+++ homework05/src/main/kotlin/ru/lpfun/spring/homework05/Homework05Application.kt
@@ -0,0 +1,11 @@
+package ru.lpfun.spring.homework05
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Homework05Application
+
+fun main(args: Array<String>) {
+	runApplication<Homework05Application>(*args)
+}
---
+++ homework05/src/test/kotlin/ru/lpfun/spring/homework05/Homework05ApplicationTests.kt
@@ -0,0 +1,13 @@
+package ru.lpfun.spring.homework05
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Homework05ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
